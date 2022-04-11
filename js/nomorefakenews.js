fbe5df0a7115a74279c86ccc43956c9a1bd558fc
aaryakini <kinia313@newschool.edu>
2022-04-11 11:54:17 -0400

    Update nomorefakenews.js

--- js/nomorefakenews.js
+++ js/nomorefakenews.js
@@ -6,13 +6,20 @@ function preload() {
 
 function setup() {
   let canvas = document.getElementById("hacked");
-}
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-function draw() {
   if (mouseIsPressed) {
     pen()
   }
 }
+
+// function draw() {
+//   if (mouseIsPressed) {
+//     pen()
+//   }
+// }
 
 function pen() {
  image(img,mouseX,mouseY,560,350);
