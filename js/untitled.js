2abd4f51e259706262be5a7ad34ac0505be8c931
aaryakini <kinia313@newschool.edu>
2022-04-11 09:52:56 -0400

    back to og

---
+++ js/untitled.js
@@ -0,0 +1,19 @@
+let img;
+
+function preload() {
+  img = loadImage('assets/images/comp.svg');
+}
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    pen()
+  }
+}
+
+function pen() {
+ image(img,mouseX,mouseY,400);
+}
