93e567d480993a4519328736bc08beda95fe5ede
aaryakini <kinia313@newschool.edu>
2022-11-08 02:12:56 -0500

    3

--- js/nomorefakenews.js
+++ js/nomorefakenews.js
@@ -56,28 +56,6 @@
 // let c = document.getElementById("canvas");
 // var ctx = c.getContext("2d");
 
-let img;
-
-function preload() {
-  img = loadImage('assets/images/comp.svg');
-}
-
-function setup() {
-  let canvas = createCanvas(window.innerWidth, 600);
-  canvas.parent('animate');
-  background("white");
-  translate(width/2 - img.width/2, height/2 - img.height/2);
-  image(img,0,0);
-  img.resize(100%,100%);
-}
-
-function draw() {
-   if (mouseIsPressed === true){
-      translate(-img.width/2, -img.height/2);
-      image(img,mouseX,mouseY);
-      img.resize(100%,100%);
-   }
-}
 
 
 
