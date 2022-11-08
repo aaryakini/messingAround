cc40e4d372beffe0004171879b5b566bcbe8a65a
aaryakini <kinia313@newschool.edu>
2022-11-08 02:29:26 -0500

    changes

--- js/nomorefakenews6.js
+++ js/nomorefakenews6.js
@@ -1,4 +1,5 @@
 let img;
+let scale = 1.5;
 
 function preload() {
   img = loadImage('assets/images/comp.svg');
@@ -8,15 +9,13 @@ function setup() {
   let canvas = createCanvas(window.innerWidth, 600);
   canvas.parent('animate');
   background("white");
-  translate(width/2 - img.width/2, height/2 - img.height/2);
-  image(img,0,0);
-  img.resize(100,100);
+  imageMode(CENTER);
+  image(img, width, height, scale*img.width, scale*img.height);
 }
 
 function draw() {
    if (mouseIsPressed === true){
-      translate(-img.width/2, -img.height/2);
-      image(img,mouseX,mouseY);
-      img.resize(100,100);
+      imageMode(CENTER);
+      image(img, mouseX, mouseY, scale*img.width, scale*img.height);
    }
 }
