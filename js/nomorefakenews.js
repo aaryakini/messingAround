26cbe2598963f92a1a86249009d31aaea6c3ffd4
aaryakini <kinia313@newschool.edu>
2022-11-08 09:28:49 -0500

    return to original

--- js/nomorefakenews.js
+++ js/nomorefakenews.js
@@ -6,57 +6,49 @@
 *
 ***************************************************************************************/
 
-// // Time for the interval to duplicate the dialog
-// var DUPLICATIONTIME = 50;
+// Time for the interval to duplicate the dialog
+var DUPLICATIONTIME = 50;
  
-// // Maxium count of windows before BSOD
-// var MAXWINDOWCOUNT = 200;
+// Maxium count of windows before BSOD
+var MAXWINDOWCOUNT = 200;
 
  
-// $(document).ready(function()
-// {
-//    // Handle for the intervalID
-//    var intervalId;
+$(document).ready(function()
+{
+   // Handle for the intervalID
+   var intervalId;
    
-//    // Make the dialog draggable
-//    $("#errordialog").draggable({
-//       handle : 'img', // Only the heading is draggable
-//       containment : 'animate', // Prevent the window getting dragged out the parent
-//       start: function(event, ui) {
-//          // Create an interval when the user starts dragging the window
-//          intervalId = setInterval("duplicateWindow()", DUPLICATIONTIME);
-//       },
-//       stop: function(event, ui) {
-//          // Clear the interval when the user stopped dragging
-//          clearInterval(intervalId)
-//       }
-//    });
-// });
+   // Make the dialog draggable
+   $("#errordialog").draggable({
+      handle : 'img', // Only the heading is draggable
+      containment : 'animate', // Prevent the window getting dragged out the parent
+      start: function(event, ui) {
+         // Create an interval when the user starts dragging the window
+         intervalId = setInterval("duplicateWindow()", DUPLICATIONTIME);
+      },
+      stop: function(event, ui) {
+         // Clear the interval when the user stopped dragging
+         clearInterval(intervalId)
+      }
+   });
+});
 
 
-// var windowCount = 0;
-// var errorDialogZIndex = 1;
-// function duplicateWindow() {
-//    // Clone the error dialog and append it to the Windows XP screen
-//    var clone = $("#errordialog").clone().appendTo('#animate');
+var windowCount = 0;
+var errorDialogZIndex = 1;
+function duplicateWindow() {
+   // Clone the error dialog and append it to the Windows XP screen
+   var clone = $("#errordialog").clone().appendTo('#animate');
    
-//    // Bring the dragging error dialog up front by changing the Z-index
-//    errorDialogZIndex++;
-//    $("#errordialog").css('z-index', errorDialogZIndex);
+   // Bring the dragging error dialog up front by changing the Z-index
+   errorDialogZIndex++;
+   $("#errordialog").css('z-index', errorDialogZIndex);
    
-//    // Check if the maximum window count has been reached
-//    windowCount++;
-//    if(windowCount == MAXWINDOWCOUNT) {
-//       $("#animate")
-//          .empty()
-//          .css('background-color', 'black');
-//    }
-// }
-
-// let c = document.getElementById("canvas");
-// var ctx = c.getContext("2d");
-
-
-
-
-
+   // Check if the maximum window count has been reached
+   windowCount++;
+   if(windowCount == MAXWINDOWCOUNT) {
+      $("#animate")
+         .empty()
+         .css('background-color', 'black');
+   }
+}
